c49f35100e0ee628acf099aed21136479b5e6367
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2018-03-24 16:01:04 +0400

    add copyright headers

--- keycloak-event-receiver-http/src/main/java/org/fintrace/keycloak/event/receiver/KeycloakEventsController.java
+++ keycloak-event-receiver-http/src/main/java/org/fintrace/keycloak/event/receiver/KeycloakEventsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,13 +21,13 @@ public class KeycloakEventsController {
         this.eventReceiver = eventReceiver;
     }
 
-    @RequestMapping(value = "/event", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/event", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
     public ResponseEntity<Void> receiveEvent(Event event) {
         eventReceiver.onEvent(event);
         return ResponseEntity.ok().build();
     }
 
-    @RequestMapping(value = "/adminevent", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/adminevent", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
     public ResponseEntity<Void> receiveAdminEvent(AdminEvent event) {
         eventReceiver.onAdminEvent(event);
         return ResponseEntity.ok().build();
